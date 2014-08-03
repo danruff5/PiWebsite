816bd29a3de1153a063ccfbdc2c9572b0a298449
Daniel Krauskopf <dckrauskopf@gmail.com>
2014-08-03 18:26:25 -0400

    Started using PHP-> need server to run

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Salvation Army</title>
+
+        <!-- Bootstrap -->
+        <link href="CSS/bootstrap.min.css" rel="stylesheet">
+
+        <link href="CSS/Main.css" rel="stylesheet" type="text/css" />
+
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <?php include '../navbar.php' ?>
+
+        <!-- jQuery -->
+        <script src="JAVASCRIPT/jquery-2.1.1.min.js"></script>
+
+        <script src="JAVASCRIPT/bootstrap.min.js"></script>
+    </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid pull-right">
+        <ul class="nav navbar-nav">
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Mission <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="../mission/statment.html">Statement</a></li>
+                    <li><a href="../mission/who.html">Who we are</a></li>
+                    <li><a href="../mission/history.html">History</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="../about/services.html">Services</a></li>
+                    <li><a href="../about/programs.html">Programs</a></li>
+                    <li><a href="../about/doctrine.html">Doctrine</a></li>
+                </ul>
+            </li>
+            <li><a href="../contact us.html" >Contact Us</a></li>
+            <li><a href="../family services.html">Family Services</a></li>
+            <li><a href="http://www.salvationarmyontariocamps.com/">Camp</a></li>
+            <li><a href="../upcoming events.html">Upcoming Events</a></li>
+            <li><a href="../salvation army at large.html">Salvation Army at Large</a></li>
+        </ul>
+    </div>
+</nav>
