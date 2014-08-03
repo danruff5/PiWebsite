6911045862da1419aa7dd9df48e496c304e507eb
Daniel Krauskopf <dckrauskopf@gmail.com>
2014-08-03 18:31:01 -0400

    Fixed PHP nav header

--- navbar.php
+++ navbar.php
@@ -1,27 +1,30 @@
-<nav class="navbar navbar-default" role="navigation">
-    <div class="container-fluid pull-right">
-        <ul class="nav navbar-nav">
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Mission <span class="caret"></span></a>
-                <ul class="dropdown-menu" role="menu">
-                    <li><a href="../mission/statment.html">Statement</a></li>
-                    <li><a href="../mission/who.html">Who we are</a></li>
-                    <li><a href="../mission/history.html">History</a></li>
-                </ul>
-            </li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us <span class="caret"></span></a>
-                <ul class="dropdown-menu" role="menu">
-                    <li><a href="../about/services.html">Services</a></li>
-                    <li><a href="../about/programs.html">Programs</a></li>
-                    <li><a href="../about/doctrine.html">Doctrine</a></li>
-                </ul>
-            </li>
-            <li><a href="../contact us.html" >Contact Us</a></li>
-            <li><a href="../family services.html">Family Services</a></li>
-            <li><a href="http://www.salvationarmyontariocamps.com/">Camp</a></li>
-            <li><a href="../upcoming events.html">Upcoming Events</a></li>
-            <li><a href="../salvation army at large.html">Salvation Army at Large</a></li>
-        </ul>
-    </div>
-</nav>
+<?php
+
+print '<nav class="navbar navbar-default" role="navigation">';
+print '    <div class="container-fluid pull-right">';
+print '        <ul class="nav navbar-nav">';
+print '            <li class="dropdown">';
+print '                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Mission <span class="caret"></span></a>';
+print '                <ul class="dropdown-menu" role="menu">';
+print '                    <li><a href="../mission/statment.html">Statement</a></li>';
+print '                    <li><a href="../mission/who.html">Who we are</a></li>';
+print '                    <li><a href="../mission/history.html">History</a></li>';
+print '                </ul>';
+print '            </li>';
+print '            <li class="dropdown">';
+print '                <a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us <span class="caret"></span></a>';
+print '                <ul class="dropdown-menu" role="menu">';
+print '                    <li><a href="../about/services.html">Services</a></li>';
+print '                    <li><a href="../about/programs.html">Programs</a></li>';
+print '                    <li><a href="../about/doctrine.html">Doctrine</a></li>';
+print '                </ul>';
+print '            </li>';
+print '            <li><a href="../contact us.html" >Contact Us</a></li>';
+print '            <li><a href="../family services.html">Family Services</a></li>';
+print '            <li><a href="http://www.salvationarmyontariocamps.com/">Camp</a></li>';
+print '            <li><a href="../upcoming events.html">Upcoming Events</a></li>';
+print '            <li><a href="../salvation army at large.html">Salvation Army at Large</a></li>';
+print '        </ul>';
+print '    </div>';
+print '</nav>';
+?>
